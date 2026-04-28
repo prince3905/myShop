9be99019a812e473d725c0330fba2c475ad194a1
harry3905 <coolfire.prince0@gmail.com>
2026-04-29 02:22:29 +0530

    fix: add MatTableModule, copy admin-layout.module from shopV3

--- client/babaShop/src/app/layouts/admin-layout/admin-layout.module.ts
+++ client/babaShop/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
-import { IonicModule } from "@ionic/angular";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AdminLayoutRoutes } from "./admin-layout.routing";
 import { DashboardComponent } from "../../dashboard/dashboard.component";
@@ -100,7 +99,6 @@ import { FactoryReportComponent } from "app/factory/factory-report/factory-repor
     MatCheckboxModule,
     MatSnackBarModule,
     MatChipsModule,
-    IonicModule,
   ],
   declarations: [
     DashboardComponent,
