3b27f97fc8ecef8ef6f48e42a6128e6a31b818db
harry3905 <coolfire.prince0@gmail.com>
2026-04-29 02:18:47 +0530

    fix: add missing Angular Material modules to app.module

--- client/babaShop/src/app/app.module.ts
+++ client/babaShop/src/app/app.module.ts
@@ -14,25 +14,41 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
 import { AuthService } from './shared/services/auth.service';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 import { LoginComponent } from './login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { BrowserModule } from '@angular/platform-browser';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatMenuModule } from '@angular/material/menu';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-
-
-
-
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 
 import { ItemService } from './shared/services/item.service';
 import { CategoryService } from './shared/services/category.service';
 import { BrandService } from './shared/services/brand.service';
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -55,8 +71,27 @@ import { MatIconModule } from '@angular/material/icon';
     MatAutocompleteModule,
     MatDialogModule,
     MatIconModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatDividerModule,
+    MatTooltipModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
     IonicModule.forRoot({ mode: 'md', hardwareBackButton: true })
-],
+  ],
   declarations: [
     AppComponent,
     AdminLayoutComponent,
@@ -65,6 +100,8 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   exports: [
     MatMenuModule,
+    MatSelectModule,
+    MatCheckboxModule,
   ],
   providers: [
     {
