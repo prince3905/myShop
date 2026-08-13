e96e08740b13bbc907dcb6d56ab3d276f56b94bb
harry3905 <coolfire.prince0@gmail.com>
2026-08-13 10:59:38 +0530

    fix(server): catch process.stdin EIO errors and add --no-stdin to nodemon to prevent startup crashes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+if (process.stdin && process.stdin.on) {
+  process.stdin.on('error', () => {});
+}
+
 const mongoose = require('mongoose');
 require("dotenv").config();
 const app = require("./app");
